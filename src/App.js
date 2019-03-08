90cef07a3c0dc74a1856ae72bd89b6cb636cc07d
Josh Leong <josh@leo.ng>
2019-03-07 22:29:11 -0700

    added icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 // import './App.css';
-import { Button } from 'evergreen-ui';
+import { Pane, Button, Text, Heading, Icon } from 'evergreen-ui';
 
 
 
@@ -9,11 +9,48 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        <Button>
-          Yay Evergreen
-        </Button>
-        </header>
+        <Pane display="flex" padding={16} background="tint2" borderRadius={3}>
+  <Pane flex={1} alignItems="center" display="flex">
+    <Heading size={600}>https://hire.thisfrontend.dev</Heading>
+  </Pane>
+  <Pane>
+    {/* Below you can see the marginRight property on a Button. */}
+    <Button marginRight={16}>Button</Button>
+    <Button appearance="primary">Primary Button</Button>
+  </Pane>
+</Pane>
+<Pane clearfix>
+  <Pane
+    elevation={0}
+    float="left"
+    backgroundColor="white"
+    width={200}
+    height={120}
+    margin={24}
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+    flexDirection="column"
+  >
+    <Text>Elevation 0</Text>
+    <Icon icon="endorsed" size={40} />
+    <Text size={300}>Flat Panes</Text>
+  </Pane>
+  <Pane
+    elevation={1}
+    float="left"
+    width={200}
+    height={120}
+    margin={24}
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+    flexDirection="column"
+  >
+    <Text>Elevation 1</Text>
+    <Text size={300}>Floating Panes</Text>
+  </Pane>
+</Pane>
       </div>
     );
   }
